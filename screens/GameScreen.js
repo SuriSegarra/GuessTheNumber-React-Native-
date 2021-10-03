34ac8dccadce76bdcd3dfc7fd4ad2b92a3fa5461
Surelis Segarra <surelissegarra@Sureliss-MacBook-Pro.local>
2021-10-03 13:52:10 -0700

    now user can give hints to computer when puts guessed number

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import React, { useState, useRef } from 'react';
+import { View, Text, StyleSheet, Button, Alert } from 'react-native';
 
 import NumberContainer from '../components/NumberContainer';
 import Card from '../components/Card';
@@ -28,13 +28,41 @@ const generateRandomBetween = (min, max, exclude) => {
 const GameScreen = props => {
     const [currentGuess, setCurrentGuess] = useState(generateRandomBetween(1, 100, props.userChoice));
 
+    // initial boundaries we are ising for the random number 
+    // this values willl not be regenerated 
+    const currentLow = useRef(1);
+    const currentHigh = useRef(100);
+
+    const nextGuessHandler = (direction) => {
+        if ((direction === 'lower' && currentGuess < props.userChoice) ||
+            (direction === 'greater' && currentGuess > props.userChoice)
+        ) {
+            Alert.alert('Don\'t lie', 'You know that this is wrong...', [
+                { text: 'Sorry!', style: 'cancel' }
+            ]);
+            return;
+        }
+        // if I am telling you, the computer that number you guessed is too big 
+        // and you should guess a lower one 
+        // then I know this number which I guessed is my currentHIgh
+        // the number which is correct cant be higher than this one 
+        // so save the number I just guessed as my current high 
+        if (direction === 'lower') {
+            currentHigh.current = currentGuess;
+        } else {
+            currentLow.current = currentGuess;
+        }
+        const nextNumber = generateRandomBetween(currentLow.current, currentHigh.current, currentGuess);
+        setCurrentGuess(nextNumber)
+    }
+
     return (
         <View style={styles.screen}>
             <Text>Opponent's Guess</Text>
             <NumberContainer>{currentGuess}</NumberContainer>
             <Card style={styles.buttonContainer}>
-                <Button title='LOWER' onPress={() => { }}></Button>
-                <Button title='GREATER' onPress={() => { }}></Button>
+                <Button title='LOWER' onPress={nextGuessHandler.bind(this, 'lower')}></Button>
+                <Button title='GREATER' onPress={nextGuessHandler.bind(this, 'greater')}></Button>
             </Card>
         </View>
     )
