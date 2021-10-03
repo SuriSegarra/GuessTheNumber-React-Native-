cbc48c82594fdd73c0ea343e0a8fc77d29b8fc2c
Surelis Segarra <surelissegarra@Sureliss-MacBook-Pro.local>
2021-10-03 16:25:37 -0700

    installed expo fonts and added the fonts to app.js

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
+import * as Font from './expo-font';
 
 import Header from './components/Header';
 import StartGameScreen from './screens/StartGameScreen';
 import GameScreen from './screens/GameScreen';
 import GamerOverScreen from './screens/GameOverScreen';
+
+const fetchFonts = () => {
+  Font.loadAsync({
+    'open-sans': required('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': required('./assets/fonts/OpenSans-Bold.ttf')
+  });
+}
 
 export default function App() {
   const [userNumber, setUserNumber] = useState();
