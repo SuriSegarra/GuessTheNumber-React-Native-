8d1dbd7f32bb15d5a9a3d7368905be590a1e2bc0
Surelis Segarra <surelissegarra@Sureliss-MacBook-Pro.local>
2021-10-03 15:57:05 -0700

    user can restart game and see the guessed number and the number of rounds

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
 
 const GamerOverScreen = props => {
-return (
-    <View style={styles.screen}>
-        <Text>The Game is Over!</Text>
-    </View>
+    return (
+        <View style={styles.screen}>
+            <Text>The Game is Over!</Text>
+            <Text>Number of rounds: {props.roundsNumber}</Text>
+            <Text>Number was: {props.userNumber}</Text>
+            <Button title='NEW GAME' onPress={props.onRestart} />
+        </View>
 
-)
+    )
 }
 
 const styles = StyleSheet.create({
