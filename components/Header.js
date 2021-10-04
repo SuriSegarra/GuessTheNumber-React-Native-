a456721977b6482a4c2b27631c95c283ff98a3ba
Surelis Segarra <surelissegarra@Sureliss-MacBook-Pro.local>
2021-10-04 13:09:54 -0700

    added image to assests folder  and imported it to GameOverScreen

--- components/Header.js
+++ components/Header.js
@@ -2,11 +2,12 @@ import React from 'react'
 import { View, Text, StyleSheet } from 'react-native';
 
 import Colors from '../constants/colors';
+import TitleText from './TitleText';
 
 const Header = props => {
     return (
         <View style={styles.header}>
-            <Text style={styles.headerTitle}>{props.title}</Text>
+            <TitleText>{props.title}</TitleText>
         </View>
     );
 };
@@ -20,11 +21,6 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center'
 
-    },
-    headerTitle: {
-        color: 'black',
-        fontSize: 18,
-        fontFamily: 'open-sans-bold'
     }
 });
 
